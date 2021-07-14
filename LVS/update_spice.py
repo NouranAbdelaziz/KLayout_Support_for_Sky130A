45c584030cd98e3a2e1c0b732377e298702cfeac
NouranAbdelaziz <79912650+NouranAbdelaziz@users.noreply.github.com>
2021-07-14 19:05:47 +0200

    Update update_spice.py

--- LVS/update_spice.py
+++ LVS/update_spice.py
@@ -1,12 +1,13 @@
 #input file
-fin = open("sky130_fd_sc_hd.spice ", "rt")
+fin = open("gpio_logic_high_cells.spice ", "rt")
 #output file to write the result to
-fout = open("sky130_fd_sc_hd_new.spice", "wt")
+fout = open("gpio_logic_high_cells_new.spice", "wt")
 #for each line in the input file
 for line in fin:
 	#read replace the string and write to output file
+    
     if (line.startswith('X')):
-        line = line.replace('X', 'M')
+        line = line.replace('X', 'M', 1)
     fout.write(line)
 #close input and output files
 fin.close()
